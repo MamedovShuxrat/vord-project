dce878353345b59a787b38903643227e639e6b3e
Shuxrat Mamedov <lider_20_10@mail.ru>
2024-07-30 22:14:39 +0300

    fix eslintrc bags

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -12,7 +12,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module"
   },
-  plugins: ["react"],
+  plugins: ["react", "prettier"],
   rules: {
     "react/react-in-jsx-scope": "off",
     quotes: ["error", "double", { allowTemplateLiterals: true }],
