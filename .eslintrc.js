7cbc57efa629385e23d7ae876b5dd11d3bb6acff
Shuxrat Mamedov <lider_20_10@mail.ru>
2024-07-31 22:44:38 +0300

    fixed small bug in eslintrc & prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "prettier",
+//     "plugin:prettier/recommended",
+//   ],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 12,
+//     sourceType: "module",
+//   },
+//   plugins: ["react", "prettier"],
+//   rules: {
+//     "react/react-in-jsx-scope": "off",
+//     quotes: ["error", "double", { allowTemplateLiterals: true }],
+//     "jsx-quotes": ["error", "prefer-double"],
+//     "react/prop-types": "off",
+//     "no-unused-vars": "warn",
+//   },
+//   globals: {
+//     process: "readonly",
+//     module: "readonly",
+//     require: "readonly",
+//   },
+//   overrides: [
+//     {
+//       files: ["webpack.config.ts", ".eslintrc.js"],
+//       env: {
+//         node: true,
+//       },
+//     },
+//   ],
+// };
+
 module.exports = {
   env: {
     browser: true,
@@ -24,6 +66,13 @@ module.exports = {
     "jsx-quotes": ["error", "prefer-double"],
     "react/prop-types": "off",
     "no-unused-vars": "warn",
+    'prettier/prettier': [
+      'warn',
+      {
+        trailingComma: 'none',  
+        endOfLine: 'auto',
+      },
+    ],
   },
   globals: {
     process: "readonly",
@@ -39,3 +88,4 @@ module.exports = {
     },
   ],
 };
+
