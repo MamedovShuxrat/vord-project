7fa0a8bc5b19cba8c6c2df63d3b0255d2ee2b4ad
Veronika <xafling@mail.ru>
2024-08-17 11:43:29 +0300

    added monaco-editor

--- src/components/Query/Query.jsx
+++ src/components/Query/Query.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
+import MonacoEditor from "@monaco-editor/react";
 import SearchBlock from "../SearchBlock/SearchBlock";
 import queryStyles from "./query.module.scss";
 
 const Query = () => {
-  const [queryText, setQueryText] = useState(""); // State for the text area
-
-  const getLineNumberedText = () => {
-    const lines = queryText.split("\n");
-    return lines.map((line, index) => `${index + 1}. ${line}`).join("\n");
-  };
+  const [queryText, setQueryText] = useState(""); // State for the SQL query text
 
   return (
     <div className={queryStyles.queryContainer}>
@@ -26,15 +22,17 @@ const Query = () => {
           showAddButton={false}
         />
       </div>
-
-      {/* Поле для ввода текста SQL-запроса с порядковыми номерами строк */}
       <div className={queryStyles.queryEditor}>
-        <pre className={queryStyles.queryTextarea}>{getLineNumberedText()}</pre>
-        <textarea
-          className={queryStyles.queryTextareaInput}
+        <MonacoEditor
+          height="400px" // Можно настроить высоту редактора
+          language="sql" // Поддержка синтаксиса SQL
+          theme="vs-light" // Темная тема, можно изменить на "light"
           value={queryText}
-          onChange={(e) => setQueryText(e.target.value)}
-          rows="20"
+          onChange={(value) => setQueryText(value || "")} // Обновляем состояние при изменении текста
+          options={{
+            lineNumbers: "on", // Включаем нумерацию строк
+            automaticLayout: true // Автоматическая настройка размеров редактора
+          }}
         />
       </div>
     </div>
